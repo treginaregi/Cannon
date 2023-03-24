59e73515ed4d234d15221bbee98d420496a71e36
julietteceb16 <julyceb16@gmail.com>
2023-03-24 12:19:13 -0600

    Update cannon.py

--- cannon.py
+++ cannon.py
@@ -23,8 +23,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 900) / 25 #se cambio la velocidada del proyectil en x 
+        speed.y = (y + 900) / 25 #se cambio la velocidada del proyectil en y
 
 
 def inside(xy):
@@ -55,7 +55,7 @@ def move():
         targets.append(target)
 
     for target in targets:
-        target.x -= 0.5
+        target.x -= 6 #se modifica la resta de pasos para que vaya más rapido
 
     if inside(ball):
         speed.y -= 0.35
